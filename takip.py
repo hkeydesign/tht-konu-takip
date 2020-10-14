bbda00ef16965bc1a96144eb8b22f84c834eb8f5
hkeydesign <68283330+hkeydesign@users.noreply.github.com>
2020-10-14 12:10:23 +0300

    Create takip.py

---
+++ takip.py
@@ -0,0 +1,36 @@
+import requests
+from lxml import html
+import time
+from win10toast import ToastNotifier
+
+
+
+temp = None
+temp_like = None
+while True:
+    
+    r = requests.get('https://www.turkhackteam.org/android-gelistirme/1945678-android-uygulama-gelistirmek-dev-konu.html')
+
+    tekil = html.fromstring(r.content)
+    tekil = tekil.xpath('body/div[@id="posts"]/div/div/div[2]/div[@style="padding:0px 0px 0px 0px"][1]/div[@class="postbit-content d-flex"]/div[2]/div/div[1]/div/div[6]/a/@href')
+    tekil = tekil[0]
+    tekill = requests.get(tekil)
+    tekill = html.fromstring(tekill.content)
+
+    thankseds = tekill.xpath('//*[@class="dbtech-ent-count"]/div[@class="thankseds"]/a/span/text()')
+    print(thankseds)
+    r = html.fromstring(r.content)
+    r = r.xpath('/html/body/div[5]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[1]/div/div[1]/text()')
+    r = r[0].strip('\n')
+    if r !=temp and temp != None:
+        print('naber')
+        bildirim = ToastNotifier()
+        bildirim.show_toast("THT","Konuna yeni bir yorum eklendi. Bakmaya ne dersin ?")
+    
+    print(r)
+    temp = r
+    if thankseds !=temp_like and temp_like != None and len(temp_like)<len(thankseds):
+        bildirim = ToastNotifier()
+        bildirim.show_toast("THT","Birisi konuna teşekkür etti ! Bakmaya ne dersin ?")
+    temp_like = thankseds
+    time.sleep(10)
